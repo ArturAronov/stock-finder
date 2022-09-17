81a071e49d779760f94bf49f908a2c380b52f2c9
ArturAronov <unionco@icloud.com>
2022-09-18 00:53:59 +0800

    set sideeffects to false

--- layouts/Earnings.js
+++ layouts/Earnings.js
@@ -24,31 +24,6 @@ const Earnings = props => {
           },
         ],
         }}
-        options={{
-          plugins: {
-            // show legends for our graph
-            legend: {
-              display: true,
-            },
-          },
-          lineHeightAnnotation: {
-            always: true,
-            lineWeight: 1.5,
-          },
-
-        //   animate in
-          animation: {
-            duration: 1,
-          },
-          maintainAspectRatio: false,
-          responsive: true,
-
-        //   show the x and y scales
-          scales: {
-            x: { display: true },
-            y: { display: true },
-          },
-        }}
         style={{maxWidth: '98%'}}
       />
     </Container>
