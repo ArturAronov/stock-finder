8daab23106cacee02be5f96dc4ca5031d4f3e73a
ArturAronov <unionco@icloud.com>
2022-09-17 20:44:21 +0800

    bug fixes for chart

--- layouts/Earnings.js
+++ layouts/Earnings.js
@@ -1,5 +1,5 @@
 import { Container } from '@chakra-ui/react'
-import { Chart as ChartJS, defaults } from 'chart.js/auto';
+import { Chart as ChartJS } from 'chart.js/auto';
 import { Chart } from 'react-chartjs-2'
 
 const Earnings = props => {
@@ -10,18 +10,22 @@ const Earnings = props => {
           datasets: [{
             type: 'line',
             label: 'Estimated Earnings',
-            data: props.estimatedData,
+            // data: props.estimatedData,
+            data: ['1', '2', '3', '4', '5'],
             borderColor: 'rgba(255, 99, 132, 1)',
           },
           {
             type: 'bar',
             label: 'Reported Earnings',
-            data: props.reportedData,
+            // data: props.reportedData,
+            data: ['1', '2', '3', '4', '5'],
             backgroundColor: 'rgba(53, 162, 235, 0.5)',
           },
         ],
-          labels: props.labels,
+          // labels: props.labels,
+          labels: ['1', '2', '3', '4', '5'],
         }}
+        style={{maxWidth: '98%'}}
       />
     </Container>
   )
