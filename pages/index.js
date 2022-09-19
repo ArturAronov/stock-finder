e671998493ce62bd62b926e5debfd9b5cd243085
ArturAronov <unionco@icloud.com>
2022-09-20 00:39:52 +0800

    removed console.log and unused components

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
-import { useEffect } from 'react'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
 import { Center } from '@chakra-ui/react'
 
 import Search from './../components/Search'
 
 export default function Home() {
-  const router = useRouter()
-  useEffect(() => {
-    console.log(router.query)
-
-  }, [router])
   return (
     <div>
       <Head>
